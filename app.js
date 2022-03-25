a98a147af3244d1ecba816cada0eb2e7c674913b
kartik patel <karishma@ladyboss.com>
2022-03-25 23:54:14 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+const validator = require('validator');
+const chalk = require('chalk');
+const notes = require('./notes');
+
+console.log(validator.isEmail('katgmail.com'));
+console.log(chalk.green.bold.bgRed('Success!'));
