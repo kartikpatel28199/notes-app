50d6c50e0a965ad7a6e8c0e5ca546315296b76c1
kartik patel <karishma@ladyboss.com>
2022-03-26 08:48:17 +0530

    cli added

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
 const validator = require('validator');
+const yargs = require('yargs');
 const chalk = require('chalk');
-const notes = require('./notes');
 
-console.log(validator.isEmail('katgmail.com'));
-console.log(chalk.green.bold.bgRed('Success!'));
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a note',
+  handler: function () {
+    console.log('Adding the note');
+  },
+});
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log('Removing the note');
+  },
+});
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List a note',
+  handler: function () {
+    console.log('List the note');
+  },
+});
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function () {
+    console.log('Reading the note');
+  },
+});
